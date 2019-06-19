98a3b69c98bc28a631b3e79088e5b4eb3612e2b5
David <dbravo7@yahoo.com>
2019-06-19 13:28:57 -0400

    Added js functions for responding for form submit, printing response to console, and loading requested images to DOM

---
+++ index.js
@@ -0,0 +1,34 @@
+"use strict"; 
+
+$(document).ready(function() {
+
+  $('form').submit(event => {
+    event.preventDefault();
+    const num = parseInt($("input[type=number]").val());
+    getDogImages(num);
+  });
+
+  function getDogImages(num) {
+    fetch(`https://dog.ceo/api/breeds/image/random/${num}`)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson));
+  }
+
+  function displayResults(responseJson) {
+    console.log(responseJson);
+    removeCurrentImages();
+
+    let count = responseJson.message.length;
+    for (let i = 0; i < count; i++) {
+
+      $('.image_container').append(
+        `<img src="${responseJson.message[i]}" class=".dog_images" alt="dog images">`
+      );
+    }
+    $('section').removeClass('hidden');
+  }
+
+  function removeCurrentImages() {
+    $('.image_container').empty(); 
+  }
+});
